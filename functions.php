c7b32862910e73c34ae52779dc100440418b83d6
William Hutt <whutt@csumb.edu>
2017-11-13 02:15:04 +0000

    Basic layout established with filter and sorting options. Next to work on filtering as is my job

--- functions.php
+++ functions.php
@@ -16,4 +16,32 @@ function displayElectronics() {
 }
 
 
+function displayAnime() {
+            $conn = getDatabaseConnection();
+            $sql = "SELECT name FROM anime";
+            // $result = $conn->query($sql);
+                
+                $stmt = $conn->prepare($sql);
+                $stmt->execute();
+                $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+               
+            //   print_r($records);
+                return $records;
+}
+
+function displayApparel() {
+            $conn = getDatabaseConnection();
+            $sql = "SELECT apparelName FROM apparrel";
+            // $result = $conn->query($sql);
+                
+                $stmt = $conn->prepare($sql);
+                $stmt->execute();
+                $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+               
+            //   print_r($records);
+                return $records;
+}
+
+
+
 ?>
--- index.php
+++ index.php
@@ -54,41 +54,24 @@
                     echo $electronic['electronicsName'] ."<br/>";
                 }
                 
+                echo "<br/>";
+                
+                $animes = displayAnime();
+                
+                foreach($animes as $anime) {
+                    echo $anime['name'] . "<br/>";
+                }
+                
+                echo "<br/>";
+                
+                $apparels = displayApparel();
+                
+                foreach($apparels as $apparel) {
+                    echo $apparel['apparelName'] . "<br/>";
+                }
                 
                 
-                
-                
-                
-                //this works
-                
-                // $servername = "us-cdbr-iron-east-05.cleardb.net";
-                // $username = "b5ba02fc3ba351";
-                // $password = "3f870355";
-                // $dbname = "heroku_860455424cb7b6b";
-                
-                // // connect to our mysql database server
-                // $conn = new mysqli($servername, $username, $password, $dbname);
-                // if ($conn->connect_error) {
-                //     die("Connection failed: " . $conn->connect_error);
-                // }
-                // // echo "Connected successfully";
-                 
-                // // make a query
-                //     $sql = "SELECT electronicsName FROM electronics WHERE 1";
-                //     $result = $conn->query($sql);
-                    
-                //     if ($result->num_rows > 0) {
-                //         // output data of each row
-                //         echo "<div id='original'>";
-                //         while ($row = $result->fetch_assoc()) {
-                //             echo $row["electronicsName"]. "<br>";
-                //             "<br>";
-                //         }
-                //         echo "</div>";
-                //     } else {
-                //         echo "0 results";
-                //     }
-                //     // $conn->close();
+
            ?>
             
         </div>
